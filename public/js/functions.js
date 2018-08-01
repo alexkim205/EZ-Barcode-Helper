db81fedc63c3082ee5d719551c3b2658ba283106
Alex Gyujin Kim <agk2144@columbia.edu>
2018-08-01 16:18:40 -0400

    create makePlate(); move everything into function

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,19 +1,23 @@
+const $ = require("jquery")
 var d3 = require("d3")
 
-var makePlate = function (svg_id, num_rows = 8, num_cols = 12, width = 600, height = 500) {
-  
-}
+var makePlate = function (svg_id, num_rows = 8, num_cols = 12,
+  width = 600, height = 500, padding = {
+    top: 100,
+    right: 100,
+    bottom: 100,
+    left: 100
+  }) {
 
-var plateData = function (num_rows = 8, num_cols = 12, width = 600, height = 500) {
-
+  // Create row and col array formulas
   // https://stackoverflow.com/questions/3895478/does-javascript-have-a-method-like-range-to-generate-a-range-within-the-supp
   function numRange(size, startAt = 0) {
     return [...Array(size).keys()].map(i => i + startAt);
   }
   // https://stackoverflow.com/questions/24597634/how-to-generate-an-array-of-alphabet-in-jquery
   function charRange(c1 = 0, c2 = 25) {
-    let a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-    return (a.slice(c1, c2));
+    let a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+    return (a.slice(c1, c2))
   }
 
   var row_labels = charRange(0, num_rows),
@@ -22,7 +26,7 @@ var plateData = function (num_rows = 8, num_cols = 12, width = 600, height = 500
   var data = new Array(),
     xpos = 1,
     ypos = 1,
-    p_width = Math.round(width/num_cols),
+    p_width = Math.round(width / num_cols),
     p_height = p_width
 
   // populate cell data
@@ -44,7 +48,74 @@ var plateData = function (num_rows = 8, num_cols = 12, width = 600, height = 500
     ypos += p_height
   }
 
-  return [col_labels, row_labels, data, Math.round(p_width/2)]
+  var svg = d3.select("#" + svg_id)
+    .attr("width", width + padding.right + padding.left)
+    .attr("height", height + padding.top + padding.bottom)
+
+  // construct rows
+  var rows = svg.selectAll("g.row")
+    .data(data)
+    .enter()
+    .append("g")
+    .attr("class", "row")
+    .attr("transform", "translate(" + padding.left + "," + padding.top + ")")
+
+  // construct columns
+  var cells = rows.selectAll("rect.cell")
+    .data(function (d) {
+      return d
+    })
+    .enter()
+    .append("rect")
+    .attr("class", "cell")
+    .attr("width", function (d) {
+      return d.width
+    })
+    .attr("height", function (d) {
+      return d.height
+    })
+    .attr("x", function (d) {
+      return d.x
+    })
+    .attr("y", function (d) {
+      return d.y
+    })
+    .style("fill", "#fff")
+    .style("stroke", "rgba(0,0,0,0.6)")
+    .on("mouseover", function (d) {
+      $(rows).find("rect.cell").attr("fill", "#fff")
+      $(this).css("fill", "#000")
+    })
+    .on("mouseout", function (d) {
+      $(this).css("fill", "#fff")
+    })
+
+  // draw labels
+  var x = d3.scalePoint()
+    .domain(col_labels)
+    .range([0, (num_cols - 1) * p_width])
+
+  var y = d3.scalePoint()
+    .domain(row_labels)
+    .range([0, (num_rows - 1) * p_width])
+
+  var xAxis = d3.axisTop()
+    .scale(x)
+
+  var yAxis = d3.axisLeft()
+    .scale(y)
+
+  var colnames = svg.append("g")
+    .attr("class", "colnames")
+    .attr("transform", "translate(" + (padding.left + Math.round(p_width / 2)) + "," + padding.top + ")")
+    .call(xAxis)
+
+  var rownames = svg.append("g")
+    .attr("class", "rownames")
+    .attr("transform", "translate(" + (padding.left) + "," + (padding.top + Math.round(p_width / 2)) + ")")
+    .call(yAxis)
+
+  return svg
 }
 
 var chooseFile = function () {
@@ -73,6 +144,6 @@ var chooseFile = function () {
 }
 
 module.exports = {
-  plateData: plateData,
+  makePlate: makePlate,
   chooseFile: chooseFile
 }
--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ require("d3-scale-chromatic")
 
 // require functions
 var {
-  plateData,
+  makePlate,
   chooseFile
 } = require("./functions")
 // require data
@@ -18,149 +18,32 @@ $(document).ready(function () {
 
   console.log("ready")
 
-  var width = 600,
-    height = 400,
+  var width = 500,
+    height = 300,
     padding = {
-      top: 100,
-      right: 100,
-      bottom: 100,
-      left: 100
+      top: 50,
+      right: 50,
+      bottom: 50,
+      left: 50
     }
 
   var file_data = chooseFile()
 
-  /* 
-   * Barcode Plate
-   */
+  /* Barcode Plate */
 
   var bc_num_rows = 6,
     bc_num_cols = 12
 
-  var [
-    bc_c_lab,
-    bc_r_lab,
-    bc_plate_data,
-    cell_width_half
-  ] = plateData(bc_num_rows, bc_num_cols, width, height)
+  var bc_svg = makePlate("svg_bc", bc_num_rows, bc_num_cols,
+    width, height, padding)
 
-  var svg_bc = d3.select("#svg_bc")
-    .attr("width", width + padding.right + padding.left)
-    .attr("height", height + padding.top + padding.bottom)
+  /* Prep Plate */
 
-  // construct rows
-  var bc_row = svg_bc.selectAll("g.row")
-    .data(bc_plate_data)
-    .enter()
-    .append("g")
-    .attr("class", "row")
-    .attr("transform", "translate(" + padding.left + "," + padding.top + ")")
+  var plate_num_rows = 8,
+    plate_num_cols = 12
 
-  // construct columns
-  var bc_cells = bc_row.selectAll("rect.cell")
-    .data(function (d) {
-      return d
-    })
-    .enter()
-    .append("rect")
-    .attr("class", "cell")
-    .attr("width", function (d) {
-      return d.width
-    })
-    .attr("height", function (d) {
-      return d.height
-    })
-    .attr("x", function (d) {
-      return d.x
-    })
-    .attr("y", function (d) {
-      return d.y
-    })
-    .style("fill", "#fff")
-    .style("stroke", "rgba(0,0,0,0.6)")
-    .on("mouseover", function (d) {
-      $(bc_row).find("rect.cell").attr("fill", "#fff")
-      $(this).css("fill", "#000")
-    })
-    .on("mouseout", function (d) {
-      $(this).css("fill", "#fff")
-    })
+  var plate_svg = makePlate("svg_plate", plate_num_rows, plate_num_cols,
+    width, height, padding)
 
-  // draw labels
-  var x = d3.scalePoint()
-    .domain(bc_c_lab)
-    .range([0, (bc_num_cols-1)*cell_width_half * 2])
-
-  var y = d3.scalePoint()
-    .domain(bc_r_lab)
-    .range([0, (bc_num_rows-1)*cell_width_half * 2])
-
-  var xAxis = d3.axisTop()
-    .scale(x)
-
-  var yAxis = d3.axisLeft()
-    .scale(y)
-
-  var bc_colnames = svg_bc.append("g")
-    .attr("class", "colnames")
-    .attr("transform", "translate(" + (padding.left + cell_width_half) + "," + padding.top + ")")
-    .call(xAxis)
-
-  var bc_rownames = svg_bc.append("g")
-    .attr("class", "rownames")
-    .attr("transform", "translate(" + (padding.left) + "," + (padding.top + cell_width_half) + ")")
-    .call(yAxis)
-
-  /* 
-   * Plate
-   */
-
-  var num_rows = 8,
-    num_cols = 12
-
-  var plate_data = plateData(num_rows, num_cols, width, height)
-
-  var svg_plate = d3.select("#svg_plate")
-    .attr("width", width + padding.right + padding.left)
-    .attr("height", height + padding.top + padding.bottom)
-
-  // var colors = d3.scaleOrdinal(d3.schemeSet3)
-
-  // construct rows
-  var row = svg_plate.selectAll("g.row")
-    .data(plate_data)
-    .enter()
-    .append("g")
-    .attr("class", "row")
-    .attr("transform", "translate(" + padding.left + "," + padding.top + ")")
-
-  // construct columns
-  row.selectAll("rect.cell")
-    .data(function (d) {
-      return d
-    })
-    .enter()
-    .append("rect")
-    .attr("class", "cell")
-    .attr("width", function (d) {
-      return d.width
-    })
-    .attr("height", function (d) {
-      return d.height
-    })
-    .attr("x", function (d) {
-      return d.x
-    })
-    .attr("y", function (d) {
-      return d.y
-    })
-    .style("fill", "#fff")
-    .style("stroke", "rgba(0,0,0,0.6)")
-    .on("mouseover", function (d) {
-      $(row).find("rect.cell").attr("fill", "#fff")
-      $(this).css("fill", "#000")
-    })
-    .on("mouseout", function (d) {
-      $(this).css("fill", "#fff")
-    })
 
 })
